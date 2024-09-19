4cf3a4c288057dac9337146b1a3f27a3fbac51aa
MdAsifFarooqui <156324502+MdAsifFarooqui@users.noreply.github.com>
2024-09-19 16:16:09 +0000

    change

--- SearchelementInArray.java
+++ SearchelementInArray.java
@@ -2,7 +2,7 @@ public class SearchelementInArray {
     static boolean isFound(int arr[],int index,int key){
         if(index==arr.length)return false;
         if(arr[index]==key)return true;
-        return isFound(arr, index+1, key);
+        return isFound(arr, index+1, key); 
     }
     public static void main(String[] args) {
         int arr[]={10,20,30,40,50};
